90cd98ff66a9150940e98a25ba88cf53b7fc2799
Professional <ustu1982@mail.ru>
2023-05-23 22:00:02 +0500

    three

--- src/MethodsHW.java
+++ src/MethodsHW.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class MethodsHW {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год: ");
+        var year = scanner.nextInt();
+        isLeapYaer(year);
+    }
+    private static void isLeapYaer(int year) {
+        if (year % 4 == 0 && (year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + "год является высокосным");
+        } else {
+            System.out.println(year + "год не является высокосным");
+        }
 
     }
+
 }

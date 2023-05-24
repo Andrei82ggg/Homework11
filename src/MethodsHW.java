37f6fb4c8e2a3630e3e3dcc0dacdf2efa5ce4620
Professional <ustu1982@mail.ru>
2023-05-24 18:31:11 +0500

    end

--- src/MethodsHW.java
+++ src/MethodsHW.java
@@ -6,7 +6,7 @@ public class MethodsHW {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("Выберите метод: \n1. Высокосный год \n2. Проверка устройства");
+            System.out.print("Выберите метод: \n1. Высокосный год \n2. Проверка устройства \n3. Количество дней");
             var type = scanner.nextInt();
 
             switch (type) {
@@ -20,6 +20,15 @@ public class MethodsHW {
                     var osType = scanner.nextInt();
                     printDevice(osType);
                     break;
+                case 3: {
+                    System.out.println("Введите дистанцию: ");
+                    var days = calculateDays(scanner.nextInt());
+                    if (days == -1) {
+                        System.out.println("Доставки нет ");
+                    } else {
+                        System.out.println("Потребуется дней: " + days);
+                    }
+                }
             }
         }
     }
@@ -32,19 +41,34 @@ public class MethodsHW {
         }
 
     }
-    private static void printDevice(int clientOS){
-        int year = LocalDate.now().getYear();
-        if (year<2015 && clientOS==0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }else if (year<2015 && clientOS==1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }else if (year>=2015&&clientOS==0){
-            System.out.println("Установите  версию приложения для iOS по ссылке");
-        }else{
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-        }
 
+    private static void printDevice(int clientOS) {
+        int year = LocalDate.now().getYear();
+        if (year < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (year < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (year >= 2015 && clientOS == 0) {
+            System.out.println("Установите  версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
+
+    private static int calculateDays(int distance) {
+        if (distance > 100) {
+            System.out.println("Доставки нет!");
+            return -1;
+        }
+        int days = 1;
+        if (distance > 20) {
+            days++;
+        }
+        if (distance > 60) {
+            days++;
+        }
+        return (days);
+    }
+}
 
 
